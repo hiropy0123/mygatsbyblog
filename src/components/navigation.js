693a64eb7f682b354d4a8568ae2d23aa76102ecd
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-09-22 20:48:33 +0900

    loop

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,14 @@ import { Link } from 'gatsby'
 // import Helmet from 'react-helmet'
 
 class Navigation extends Component {
+  items = [
+    { path: '/', name: 'Home' },
+    { path: '/about/', name: 'About' },
+    { path: '/works/', name: 'Works' },
+    { path: '/access/', name: 'Access' },
+    { path: '/contact/', name: 'Contact' }
+  ]
+
   styles = {
     nav: {
       position: 'absolute',
@@ -19,26 +27,19 @@ class Navigation extends Component {
   }
 
 
-
   render() {
     return (
       <nav style={this.styles.nav}>
         <ul style={this.styles.ul}>
-          <li style={this.styles.li}>
-            <Link to='/'>Home</Link>
-          </li>
-          <li style={this.styles.li}>
-            <Link to='/about/'>About</Link>
-          </li>
-          <li style={this.styles.li}>
-            <Link to='/works/'>Works</Link>
-          </li>
-          <li style={this.styles.li}>
-            <Link to='/access/'>Access</Link>
-          </li>
-          <li style={this.styles.li}>
-            <Link to='/contact/'>Contact</Link>
-          </li>
+        { 
+          this.items.map((data, index) => {
+            return (
+              <li style={this.styles.li}>
+                <Link to={data.path} key={index}>{data.name}</Link>
+              </li>
+            )
+          })
+        }
         </ul>
       </nav>
     )
