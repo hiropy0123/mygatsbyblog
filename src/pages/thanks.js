63a47c93b253fbcd487f2a4255d3a36f3d6ac265
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-10-26 15:19:30 +0900

    mail form test

---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const ThanksPage = () => (
+  <Layout>
+    <h1>Your Email sent!</h1>
+    <p>お問い合わせありがとうございました。</p>
+    <Link to="/" exact>Top</Link>
+  </Layout>
+)
+
+export default ThanksPage
