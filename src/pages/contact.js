eb764471a82d5325879a7f5e2ddcdaa41b7fee9c
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-10-27 18:40:11 +0900

    spell

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,29 +10,29 @@ const ContactPage = () => (
     <form 
       name="contact" 
       method="POST" 
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
+      dataNetlify="true"
+      dataNetlifyHoneypot="bot-field"
     >
     <input type="hidden" name="form-name" value="contact" />
     <input type="hidden" name="bot-field" />
 
-      <div classList="form-group">
+      <div className="form-group">
         <label>お名前<abbr title="required">*</abbr>
-        <input type="text" class="form-control" id="name" name="name" placeholder="お名前" maxlength="30" minlength="2" required autocomplete="name" />
+        <input type="text" className="form-control" id="name" name="name" placeholder="お名前" maxlength="30" minlength="2" required autocomplete="name" />
         </label>
       </div>
-      <div classList="form-group">
+      <div className="form-group">
         <label>メールアドレス<abbr title="required">*</abbr>
-        <input type="email" class="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autocomplete="email" />
+        <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autocomplete="email" />
         </label>
       </div>
-      <div classList="form-group">
+      <div className="form-group">
         <label>お問い合わせ内容<abbr title="required">*</abbr>
-        <textarea class="form-control" id="contact" name="content" rows="8" required></textarea>
+        <textarea className="form-control" id="contact" name="content" rows="8" required></textarea>
         </label>
       </div>
 
-      <div classList="form-group">
+      <div className="form-group">
       <button type="submit">Send</button>
       </div>
     </form>
